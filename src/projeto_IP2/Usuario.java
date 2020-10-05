df250ed3adf6a266e0ffe075737d2411b91236d1
Gabriel Gomes <gabrielgomesdesantana@gmail.com>
2020-10-05 01:48:45 -0300

    Criada a classe Usuario e adicionado metodos get e set

---
+++ src/projeto_IP2/Usuario.java
@@ -0,0 +1,78 @@
+package projeto_IP2;
+
+public class Usuario {
+	//ATRIBUTOS
+	private String nome;
+	private String senha;
+	private String email;
+	private int pontuacao;
+	private String pais;
+	private String genero;
+	private int idade;
+
+	//CONSTRUCTOR
+	public Usuario() {
+		
+	}
+
+	
+	//METODOS GET E SET
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getPontuacao() {
+		return pontuacao;
+	}
+
+	public void setPontuacao(int pontuacao) {
+		this.pontuacao = pontuacao;
+	}
+
+	public String getPais() {
+		return pais;
+	}
+
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	
+
+}
